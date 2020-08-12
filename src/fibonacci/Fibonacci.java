3b64b4f7305f5b5c831b1f605c2c58b193887e41
mancpato <manc@uabcs.mx>
2020-08-11 21:17:41 -0600

    Pocos cambios
    Edición menor, solo para probar

--- src/fibonacci/Fibonacci.java
+++ src/fibonacci/Fibonacci.java
@@ -1,7 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+        Jesus Ivan Beltran Moreno
  */
 package fibonacci;
 
@@ -9,17 +7,9 @@ import java.util.ArrayList; //no la use
 import java.util.Scanner;
         
 
-/**
- *
- * @author ivanc
- */
 public class Fibonacci {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         Scanner sc = new Scanner(System.in);
         int Numero_Izq = 0; 
         int Numero_Der = 1;
